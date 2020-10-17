851833fb1e616f53af0f0aa13c5e7255d3c961b1
Julian <juliandomke@outlook.de>
2020-10-17 13:02:56 +0200

    Added gitignore

---
+++ build/precache-manifest.dac2cd89b2ddb56165d12a1195125d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e323cefee568a0f73065f5a5f814673f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abae7caf4bd881f18b39",
+    "url": "/static/css/main.85d8b8cc.chunk.css"
+  },
+  {
+    "revision": "41896e867a49c5c3a67a",
+    "url": "/static/js/2.460ea015.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/static/js/2.460ea015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abae7caf4bd881f18b39",
+    "url": "/static/js/main.fd024dc1.chunk.js"
+  },
+  {
+    "revision": "97c7e3dce7962e6e78db",
+    "url": "/static/js/runtime-main.edc19d88.js"
+  }
+]);
